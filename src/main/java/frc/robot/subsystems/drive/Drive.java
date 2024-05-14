00c53cd727ea1436f34225ad4d5657f5356fe5c0
MrTinker64 <avi.micah@gmail.com>
2024-05-14 15:52:59 -0700

    changed how it is getting logged to be more consistent

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -157,7 +157,7 @@ public class Drive extends SubsystemBase {
       Twist2d twist = kinematics.toTwist2d(moduleDeltas);
       rawGyroRotation = rawGyroRotation.plus(new Rotation2d(twist.dtheta));
     }
-    Logger.recordOutput("Odometry/rawGyroRotationDegrees", rawGyroRotation.getDegrees());
+    Logger.recordOutput("Drive/rawGyroRotationDegrees", rawGyroRotation.getDegrees());
 
     // Apply odometry update
     poseEstimator.update(rawGyroRotation, modulePositions);
