52887997ce225e3ec8e776b82eae38f338fae55f
MrTinker64 <avi.micah@gmail.com>
2024-05-22 21:09:40 -0700

    formatting bluh

--- src/main/java/frc/robot/subsystems/drive/ModuleIOMixed.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOMixed.java
@@ -27,7 +27,6 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 
@@ -125,22 +124,14 @@ public class ModuleIOMixed implements ModuleIO {
     BaseStatusSignal.setUpdateFrequencyForAll(
         100.0, drivePosition); // Required for odometry, use faster rate
     BaseStatusSignal.setUpdateFrequencyForAll(
-        50.0,
-        driveVelocity,
-        driveAppliedVolts,
-        driveCurrent,
-        turnAbsolutePosition);
+        50.0, driveVelocity, driveAppliedVolts, driveCurrent, turnAbsolutePosition);
     driveTalon.optimizeBusUtilization();
   }
 
   @Override
   public void updateInputs(ModuleIOInputs inputs) {
     BaseStatusSignal.refreshAll(
-        drivePosition,
-        driveVelocity,
-        driveAppliedVolts,
-        driveCurrent,
-        turnAbsolutePosition);
+        drivePosition, driveVelocity, driveAppliedVolts, driveCurrent, turnAbsolutePosition);
 
     inputs.drivePositionRad =
         Units.rotationsToRadians(drivePosition.getValueAsDouble()) / DRIVE_GEAR_RATIO;
