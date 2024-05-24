8c8c05ba70ca559c19f56b54c596d40a48718a2b
MrTinker64 <avi.micah@gmail.com>
2024-05-24 08:43:05 -0700

    made pivot sim

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -2,4 +2,19 @@ package frc.robot.subsystems.intake;
 
 import static frc.robot.subsystems.intake.IntakeConstants.*;
 
-public class IntakeIOSim {}
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+public class IntakeIOSim {
+    private final SingleJointedArmSim pivotSim =
+      new SingleJointedArmSim(
+          DCMotor.getNEO(1),
+          100,
+          1.06328,
+          Units.inchesToMeters(13.835),
+          Math.toRadians(0.0),
+          Math.toRadians(125.0),
+          false,
+          Units.degreesToRadians(0.0));
+}
