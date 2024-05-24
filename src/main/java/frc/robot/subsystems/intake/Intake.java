4cbed473bd1b6c9850a126e54e82fa6086d04e3f
MrTinker64 <avi.micah@gmail.com>
2024-05-23 18:33:49 -0700

    made the logged tunable numbers

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -7,6 +7,10 @@ import frc.robot.util.LoggedTunableNumber;
 public class Intake {
   private static final LoggedTunableNumber kP = new LoggedTunableNumber("Intake/Gains/kP", gains.kP());
 
+  private static final LoggedTunableNumber loweredAngle = new LoggedTunableNumber("Intake/Angles/lowered", kLoweredAngle);
+  private static final LoggedTunableNumber raisedAngle = new LoggedTunableNumber("Intake/Angles/raised", kRaisedAngle);
+  private static final LoggedTunableNumber climbingAngle = new LoggedTunableNumber("Intake/Angles/climbing", kClimbingAngle);
+
   private final IntakeIO io;
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 }
