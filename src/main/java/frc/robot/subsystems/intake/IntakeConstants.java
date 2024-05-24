0f7cb487d56cb51457ddf706c2c684442c3405ea
MrTinker64 <avi.micah@gmail.com>
2024-05-23 18:22:48 -0700

    added CAN IDs

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -3,11 +3,14 @@ package frc.robot.subsystems.intake;
 import frc.robot.Constants;
 
 public class IntakeConstants {
-  public static final Gains gains =
-      switch (Constants.currentMode) {
-        default -> new Gains(90.0);
-        case SIM -> new Gains(75.0);
-      };
+    public static final int angleMotorId = 9;
+    public static final int rollersMotorId = 10;
 
-  public record Gains(double kP) {}
+    public static final Gains gains =
+        switch (Constants.currentMode) {
+            default -> new Gains(90.0);
+            case SIM -> new Gains(75.0);
+        };
+
+    public record Gains(double kP) {}
 }
