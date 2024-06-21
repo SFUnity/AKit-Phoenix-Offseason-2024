dd5cf7c03fb3608ac78ffc761dfc1747aebf1ebf
MrTinker64 <avi.micah@gmail.com>
2024-06-21 18:01:17 +0200

    formatting :|

--- src/main/java/frc/robot/subsystems/vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -3,23 +3,23 @@ package frc.robot.subsystems.vision;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface VisionIO {
-    @AutoLog
-    public static class VisionIOInputs {
-        public double targetXOffset = 0;
-        public double targetYOffset = 0;
-        public double targetIsDetected = 0;
-        public double targetArea = 0;
-        public double targetID = 0;   // For aprilTags
-        public double priorityID = 0; // For aprilTags
-        public double pipeline = 0; // TODO store the pipelines where the code can see them
-        
-        public double ledMode = 0;
-        public double camMode = 0;
-    }
+  @AutoLog
+  public static class VisionIOInputs {
+    public double targetXOffset = 0;
+    public double targetYOffset = 0;
+    public double targetIsDetected = 0;
+    public double targetArea = 0;
+    public double targetID = 0; // For aprilTags
+    public double priorityID = 0; // For aprilTags
+    public double pipeline = 0; // TODO store the pipelines where the code can see them
 
-    /** Updates the set of loggable inputs. */
-    public default void updateInputs(VisionIOInputs inputs) {}
+    public double ledMode = 0;
+    public double camMode = 0;
+  }
 
-    /** Sets the pipeline number. */
-    public default void setPipeline(int pipeline) {}
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(VisionIOInputs inputs) {}
+
+  /** Sets the pipeline number. */
+  public default void setPipeline(int pipeline) {}
 }
