2e2ac08983b1b47e6e31eaa36c300d62f9772b9b
MrTinker64 <avi.micah@gmail.com>
2024-06-22 08:13:56 +0200

    finished the AKit stuff for the vision subsystem

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,10 +1,19 @@
 package frc.robot.subsystems.vision;
 
-public class Vision {
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
   private final VisionIO io;
   private final VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
 
   public Vision(VisionIO io) {
     this.io = io;
+  }
+
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Vision", inputs);
   }
 }
