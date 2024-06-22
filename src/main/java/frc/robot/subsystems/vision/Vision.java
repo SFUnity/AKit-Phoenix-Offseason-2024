e5eb4c8e7cc63eedfb169a797bfa48b65573eaec
MrTinker64 <avi.micah@gmail.com>
2024-06-22 11:20:12 +0200

    added all the methods from OG limelight class

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems.vision;
 
-import org.littletonrobotics.junction.Logger;
+import static frc.robot.subsystems.vision.VisionConstants.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class Vision extends SubsystemBase {
   private final VisionIO io;
@@ -10,10 +11,27 @@ public class Vision extends SubsystemBase {
 
   public Vision(VisionIO io) {
     this.io = io;
+
+    io.setPipeline(Pipelines.BLUE_SPEAKER.get());
   }
 
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Vision", inputs);
+  }
+
+  public boolean alignedWithTag() {
+    if (inputs.targetDetected) {
+      System.out.println("No tag in sight"); // TODO make an Alert
+    }
+    return Math.abs(inputs.targetXOffset) < 2;
+  }
+
+  public double getDistance () {
+    double angleToGoalDegrees = limelightMountAngleDegrees + inputs.targetYOffset;
+    double angleToGoalRadians = Math.toRadians(angleToGoalDegrees);
+
+    // calculate distance
+    return (heightOfTagInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
   }
 }
