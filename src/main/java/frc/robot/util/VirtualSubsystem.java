f31f24040c60970f6a07190c3578ac934956e102
MrTinker64 <avi.micah@gmail.com>
2024-06-24 18:45:04 +0200

    took mechanical advantage's 2024 led class

---
+++ src/main/java/frc/robot/util/VirtualSubsystem.java
@@ -0,0 +1,27 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class VirtualSubsystem {
+  private static List<VirtualSubsystem> subsystems = new ArrayList<>();
+
+  public VirtualSubsystem() {
+    subsystems.add(this);
+  }
+
+  public static void periodicAll() {
+    for (VirtualSubsystem subsystem : subsystems) {
+      subsystem.periodic();
+    }
+  }
+
+  public abstract void periodic();
+}
