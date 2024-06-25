2e1c2b5777ee4d9e6c845f20ed2e7664cd5c3414
MrTinker64 <avi.micah@gmail.com>
2024-06-25 19:23:48 +0200

    it's perfect

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -3,16 +3,13 @@ package frc.robot.subsystems.vision;
 import static frc.robot.subsystems.vision.VisionConstants.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.leds.Leds;
 import frc.robot.subsystems.vision.VisionConstants.Pipelines;
-import frc.robot.util.Alert;
-import frc.robot.util.Alert.AlertType;
 import org.littletonrobotics.junction.Logger;
 
 public class Vision extends SubsystemBase {
   private final VisionIO io;
   private final VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
-
-  private final Alert noTagDetectedAlert = new Alert("No tag detected", AlertType.WARNING);
 
   public Vision(VisionIO io) {
     this.io = io;
@@ -24,7 +21,7 @@ public class Vision extends SubsystemBase {
     io.updateInputs(inputs);
     Logger.processInputs("Vision", inputs);
 
-    noTagDetectedAlert.set(!inputs.targetDetected); // TODO remove once LEDs are added
+    Leds.getInstance().targetDetected = inputs.targetDetected;
   }
 
   public boolean alignedWithTag() {
