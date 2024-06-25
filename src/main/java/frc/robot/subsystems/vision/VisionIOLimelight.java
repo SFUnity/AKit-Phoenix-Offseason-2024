f0a7d571091df08572171affe960435bcbec164e
MrTinker64 <avi.micah@gmail.com>
2024-06-25 09:03:58 +0200

    added a disconnect alert to limelightIO
    TODO figure out how to check if the limelight is disconnected

--- src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.vision;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.util.Alert;
 
 public class VisionIOLimelight implements VisionIO {
   private static NetworkTable table;
@@ -10,13 +12,15 @@ public class VisionIOLimelight implements VisionIO {
   private static NetworkTableEntry camMode;
   private static NetworkTableEntry ledMode;
 
-  public VisionIOLimelight() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-    
-    tx =
-        table.getEntry("tx"); // Horizontal offset from crosshair to target (-29.8 to 29.8 degrees).
-    ty =
-        table.getEntry("ty"); // Vertical offset from crosshair to target (-24.85 to 24.85 degrees).
+  private static final double disconnectedTimeout = 0.5;
+  private final Alert disconnectedAlert;
+  private final Timer disconnectedTimer = new Timer();
+
+  public VisionIOLimelight(String name) {
+    table = NetworkTableInstance.getDefault().getTable(name);
+
+    tx = table.getEntry("tx"); // Horizontal offset from crosshair to target (-29.8 to 29.8 degrees)
+    ty = table.getEntry("ty"); // Vertical offset from crosshair to target (-24.85 to 24.85 degrees)
     tv = table.getEntry("tv"); // Whether the limelight has any valid targets (0 or 1).
     ta = table.getEntry("ta"); // Target area (0% of image to 100% of image).
     tid = table.getEntry("tid"); // current id of the april tag
@@ -25,6 +29,10 @@ public class VisionIOLimelight implements VisionIO {
     pipeline = table.getEntry("pipeline"); // Pipeline (0-9).
     ledMode = table.getEntry("ledMode"); // limelight's LED state (0-3).
     camMode = table.getEntry("camMode"); // limelight's operation mode (0-1).
+
+    disconnectedAlert =
+        new Alert("No data from: " + name, Alert.AlertType.ERROR);
+    disconnectedTimer.start();
   }
 
   @Override
@@ -39,6 +47,10 @@ public class VisionIOLimelight implements VisionIO {
     inputs.pipeline = pipeline.getDouble(0);
     inputs.ledMode = ledMode.getDouble(0);
     inputs.camMode = camMode.getDouble(0);
+
+    // Update disconnected alert
+    // TODO check how to see the if limelight is disconnected
+    disconnectedAlert.set(disconnectedTimer.hasElapsed(disconnectedTimeout));
   }
 
   @Override
