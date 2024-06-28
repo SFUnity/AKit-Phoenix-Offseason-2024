1d24247442313983e3b7eea0888d0eeb2c0d2b15
MrTinker64 <avi.micah@gmail.com>
2024-06-28 14:11:16 +0200

    name change

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -145,7 +145,7 @@ public class Intake extends SubsystemBase {
                 rollersStop();
                 indexerStop();
               })
-          .withName("raiseAndStop");
+          .withName("raise and stop");
     } else {
       return runOnce(() -> {});
     }
