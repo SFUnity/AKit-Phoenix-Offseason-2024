b57daed198c6009ae13081fdf38c6ea61a3d7dfa
MrTinker64 <avi.micah@gmail.com>
2024-06-28 15:07:36 +0200

    removed unnecessary imports + formatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,8 +18,6 @@ import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -161,9 +159,7 @@ public class RobotContainer {
     SmartDashboard.putData(intake.intakeCmd(true));
   }
 
-  /**
-   * Use this method to define your button->command mappings.
-   */
+  /** Use this method to define your button->command mappings. */
   private void configureButtonBindings() {
     // Default cmds
     drive.setDefaultCommand(
