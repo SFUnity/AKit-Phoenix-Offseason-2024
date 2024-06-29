e124587493a00c09be2ac8e8db88fc96d9878019
MrTinker64 <avi.micah@gmail.com>
2024-06-29 08:12:48 -0700

    added an easy version of position and size

--- src/main/java/frc/robot/util/loggedShuffleboardClasses/LoggedShuffleboardButton.java
+++ src/main/java/frc/robot/util/loggedShuffleboardClasses/LoggedShuffleboardButton.java
@@ -55,7 +55,56 @@ public class LoggedShuffleboardButton implements LoggedDashboardInput {
     periodic();
     Logger.registerDashboardInput(this);
   }
-  // TODO figure out how to have setPosition
+  // TODO figure out better way to do position and size
+
+  /**
+   * Creates a new LoggedShuffleboardBoolean, for handling a string input sent via NetworkTables.
+   *
+   * @param key The key for the boolean, published to "/SmartDashboard/{key}" for NT or
+   *     "/DashboardInputs/{key}" when logged.
+   * @param tab name of the Shuffleboard tab you want the button to appear in
+   * @param defaultValue The default value if no value in NT is found.
+   * @param width The width of the button
+   * @param height The height of the button
+   */
+  public LoggedShuffleboardButton(String key, String tab, boolean defaultValue, int width, int height) {
+    this.key = key;
+    this.defaultValue = defaultValue;
+    this.value = defaultValue;
+    this.entry =
+        Shuffleboard.getTab(tab)
+            .add(key, defaultValue)
+            .withWidget(BuiltInWidgets.kToggleButton)
+            .withSize(width, height)
+            .getEntry();
+    periodic();
+    Logger.registerDashboardInput(this);
+  }
+
+  /**
+   * Creates a new LoggedShuffleboardBoolean, for handling a string input sent via NetworkTables.
+   *
+   * @param key The key for the boolean, published to "/SmartDashboard/{key}" for NT or
+   *     "/DashboardInputs/{key}" when logged.
+   * @param tab name of the Shuffleboard tab you want the button to appear in
+   * @param defaultValue The default value if no value in NT is found.
+   * @param width The width of the button
+   * @param height The height of the button
+   */
+  public LoggedShuffleboardButton(String key, String tab, boolean defaultValue, int width, int height, int column, int row) {
+    this.key = key;
+    this.defaultValue = defaultValue;
+    this.value = defaultValue;
+    this.entry =
+        Shuffleboard.getTab(tab)
+            .add(key, defaultValue)
+            .withWidget(BuiltInWidgets.kToggleButton)
+            .withSize(width, height)
+            .withPosition(column, row)
+            .getEntry();
+    periodic();
+    Logger.registerDashboardInput(this);
+  }
 
   /** Updates the default value, which is used if no value in NT is found. */
   public void setDefault(boolean defaultValue) {
