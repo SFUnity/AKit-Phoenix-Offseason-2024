0c5edf3eb92e94c98382e69876626d4671db915f
MrTinker64 <avi.micah@gmail.com>
2024-07-02 07:39:01 -0700

    add docs

--- src/main/java/frc/robot/util/loggedShuffleboardClasses/LoggedShuffleboardBoolean.java
+++ src/main/java/frc/robot/util/loggedShuffleboardClasses/LoggedShuffleboardBoolean.java
@@ -30,7 +30,7 @@ public class LoggedShuffleboardBoolean implements LoggedDashboardInput {
   /**
    * Creates a new LoggedShuffleboardBoolean, for handling a string input sent via NetworkTables.
    *
-   * @param key The key for the boolean, published to "/SmartDashboard/{key}" for NT or
+   * @param key The key for the boolean, published to "/Shuffleboard/{tab}/{key}" for NT or
    *     "/DashboardInputs/{key}" when logged.
    * @param tab name of the Shuffleboard tab you want the button to appear in
    * @param defaultValue The default value if no value in NT is found.
@@ -45,18 +45,38 @@ public class LoggedShuffleboardBoolean implements LoggedDashboardInput {
     Logger.registerDashboardInput(this);
   }
 
+  /**
+   * Sets the type of widget used to display the data. If not set, the default widget type will be
+   * used.
+   *
+   * @param widgetType the type of the widget used to display the data
+   */
   public LoggedShuffleboardBoolean withWidget(WidgetType widgetType) {
     widget = widget.withWidget(widgetType);
     return this;
   }
 
+  /**
+   * Sets the size of this component in the tab. This has no effect if this component is inside a
+   * layout.
+   *
+   * @param width how many columns wide the component should be
+   * @param height how many rows high the component should be
+   */
   public LoggedShuffleboardBoolean withSize(int width, int height) {
     widget = widget.withSize(width, height);
     return this;
   }
 
-  public LoggedShuffleboardBoolean withPosition(int x, int y) {
-    widget = widget.withPosition(x, y);
+  /**
+   * Sets the position of this component in the tab. This has no effect if this component is inside
+   * a layout.
+   *
+   * @param columnIndex the column in the tab to place this component
+   * @param rowIndex the row in the tab to place this component
+   */
+  public LoggedShuffleboardBoolean withPosition(int columnIndex, int rowIndex) {
+    widget = widget.withPosition(columnIndex, rowIndex);
     return this;
   }
 
