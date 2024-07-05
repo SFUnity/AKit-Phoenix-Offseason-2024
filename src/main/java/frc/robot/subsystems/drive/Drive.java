56f1991a6ff146ab5b6f175679c9abe0d9e8f35c
MrTinker64 <avi.micah@gmail.com>
2024-07-04 17:03:34 -0700

    Revert "add delta wheel odom filter"
    ...because it was " a quick-and-dirty solution" and now there are better ways to do it
    
    This reverts commit d89e1a4508746ba3acad1ce580b98f70991b910a.

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -58,9 +58,6 @@ public class Drive extends SubsystemBase {
   private final Module[] modules = new Module[4]; // FL, FR, BL, BR
   private final SysIdRoutine sysId;
 
-  // Store previous positions and time for filtering odometry data
-  private double lastTime = 0.0;
-
   private SwerveDriveKinematics kinematics = new SwerveDriveKinematics(getModuleTranslations());
   private Rotation2d rawGyroRotation = new Rotation2d();
   private SwerveModulePosition[] lastModulePositions = // For delta tracking
@@ -167,6 +164,7 @@ public class Drive extends SubsystemBase {
               modulePositions[moduleIndex].distanceMeters
                   - lastModulePositions[moduleIndex].distanceMeters,
               modulePositions[moduleIndex].angle);
+      lastModulePositions[moduleIndex] = modulePositions[moduleIndex];
     }
 
     // Update gyro angle
@@ -179,32 +177,8 @@ public class Drive extends SubsystemBase {
       rawGyroRotation = rawGyroRotation.plus(new Rotation2d(twist.dtheta));
     }
 
-    // Filter odometry update based on delta wheel positions
-    double currentTime = Timer.getFPGATimestamp();
-    boolean includeMeasurement = true;
-    if (lastModulePositions != null) {
-      double dt = currentTime - lastTime;
-      for (int j = 0; j < modules.length; j++) {
-        double velocity = moduleDeltas[j].distanceMeters / dt;
-        double omega = moduleDeltas[j].angle.getRadians() / dt;
-        // Check if delta is too large
-        if (Math.abs(omega)
-                > Units.degreesToRadians(1080.0) * 5.0 // Max steering velocity = 1080 deg/sec
-            || Math.abs(velocity) > MAX_LINEAR_SPEED * 5.0) { // Max drive velocity = 14.5 ft/sec
-          includeMeasurement = false;
-          break;
-        }
-      }
-    }
-
-    // If delta isn't too large we can include the measurement.
-    if (includeMeasurement) {
-      lastModulePositions = modulePositions;
-      lastTime = currentTime;
-      poseEstimator.update(rawGyroRotation, modulePositions);
-    }
-
-    Logger.recordOutput("Drive/includeMeasurement", includeMeasurement);
+    // Apply odometry update
+    poseEstimator.update(rawGyroRotation, modulePositions);
   }
 
   /**
