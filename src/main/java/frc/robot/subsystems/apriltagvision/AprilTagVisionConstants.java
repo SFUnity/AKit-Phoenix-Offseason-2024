c005c251b39385b3ec1d2329da8367a28ead75a0
MrTinker64 <avi.micah@gmail.com>
2024-07-05 11:16:37 -0700

    add check for being on field

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionConstants.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionConstants.java
@@ -1,15 +1,15 @@
 package frc.robot.subsystems.apriltagvision;
 
 public class AprilTagVisionConstants {
+  public static final double fieldBorderMargin = 0.5;
+
   /** how many degrees back is your limelight rotated from perfectly vertical? */
   public static final double limelightMountAngleDegrees = 28.0;
-
   /** distance from the center of the Limelight lens to the floor */
   public static final double limelightLensHeightInches = 26.5;
 
   /** distance from the speaker tag to the floor */
   public static final double heightOfTagInches = 56.5;
-
   /** height of speaker */
   public static final double heightOfSpeakerInches = 80;
 
