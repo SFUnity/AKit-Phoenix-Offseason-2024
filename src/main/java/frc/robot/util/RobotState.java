f481c74050bbc31e112225ef7b8ae12d610afc8a
MrTinker64 <avi.micah@gmail.com>
2024-07-06 16:22:45 -0700

    rename Pose Manager to Robot State for clarity

--- src/main/java/frc/robot/util/RobotState.java
+++ src/main/java/frc/robot/util/RobotState.java
@@ -1,15 +1,23 @@
 package frc.robot.util;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
-public class PoseManager {
+public class RobotState {
   public static final double TRACK_WIDTH_X = Units.inchesToMeters(22.75); // Check when copying
   public static final double TRACK_WIDTH_Y = Units.inchesToMeters(22.75);
   public static final double DRIVE_BASE_RADIUS =
       Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
 
-  public PoseManager() {}
+  private SwerveDriveKinematics kinematics =
+      new SwerveDriveKinematics(getModuleTranslations());
+
+  public RobotState() {}
+
+  public SwerveDriveKinematics getKinematics() {
+    return kinematics;
+  }
 
   /** Returns an array of module translations. */
   public static Translation2d[] getModuleTranslations() {
