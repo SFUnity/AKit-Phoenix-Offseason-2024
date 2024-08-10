d7a50168751d4baaf2226a7ca293bde169b96b25
MrTinker64 <avi.micah@gmail.com>
2024-08-09 18:17:54 -0700

    remove all getCharacterizationVelocity methods

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -187,9 +187,4 @@ public class Module {
   public SwerveModuleState getState() {
     return new SwerveModuleState(getVelocityMetersPerSec(), getAngle());
   }
-
-  /** Returns the drive velocity in radians/sec. */
-  public double getCharacterizationVelocity() {
-    return inputs.driveVelocityRadPerSec;
-  }
 }
