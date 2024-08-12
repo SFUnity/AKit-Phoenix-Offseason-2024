6fc296349ceeaaf936a3d96edcc6cbde1d4782c0
MrTinker64 <avi.micah@gmail.com>
2024-08-11 20:02:41 -0700

    add dynamic cropping

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionIOLimelight.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.apriltagvision;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.apriltagvision.AprilTagVisionConstants.*;
 import frc.robot.util.Alert;
@@ -14,6 +15,9 @@ public class AprilTagVisionIOLimelight implements AprilTagVisionIO {
   private final Alert disconnectedAlert;
   private double lastTimestamp = 0;
 
+  private final double DEFAUlT_CROP = 0.9;
+  private final double CROP_BUFFER = 0.1;
+
   public AprilTagVisionIOLimelight(String camName, PoseManager poseManager) {
     name = camName;
     this.poseManager = poseManager;
@@ -21,6 +25,8 @@ public class AprilTagVisionIOLimelight implements AprilTagVisionIO {
     LimelightHelpers.setLEDMode_PipelineControl(name);
 
     disconnectedAlert = new Alert("No data from: " + name, Alert.AlertType.ERROR);
+
+    resetCropping();
   }
 
   @Override
@@ -42,6 +48,8 @@ public class AprilTagVisionIOLimelight implements AprilTagVisionIO {
       lastTimestamp = observation.timestampSeconds;
     }
     disconnectedAlert.set(Timer.getFPGATimestamp() - lastTimestamp < disconnectedTimeout);
+
+    dynamicCropping();
   }
 
   @Override
@@ -52,5 +60,40 @@ public class AprilTagVisionIOLimelight implements AprilTagVisionIO {
   @Override
   public void setPipeline(Pipelines pipelineEnum) {
     LimelightHelpers.setPipelineIndex(name, Pipelines.getIndexFor(pipelineEnum));
+  }
+
+  private void dynamicCropping() {
+    double[] tcornxy = LimelightHelpers.getLimelightNTDoubleArray(name, "tcornxy");
+    if (tcornxy.length == 0) {
+      resetCropping();
+      return;
+    }
+
+    double minX = 0;
+    double maxX = 0;
+    double minY = 0;
+    double maxY = 0;
+
+    // Iterate over all tag corners
+    for (int i = 0; i < tcornxy.length / 2; i+=2) {
+        minX = Math.min(minX, tcornxy[i]);
+        maxX = Math.max(maxX, tcornxy[i]);
+    }
+    for (int i = 1; i < tcornxy.length / 2; i+=2) {
+        minY = Math.min(minY, tcornxy[i]);
+        maxY = Math.max(maxY, tcornxy[i]);
+    }
+
+    // Apply crop buffer and clamp to default crop size
+    double cropXMin = MathUtil.clamp(minX - CROP_BUFFER, -DEFAUlT_CROP, DEFAUlT_CROP);
+    double cropXMax = MathUtil.clamp(maxX + CROP_BUFFER, -DEFAUlT_CROP, DEFAUlT_CROP);
+    double cropYMin = MathUtil.clamp(minY - CROP_BUFFER, -DEFAUlT_CROP, DEFAUlT_CROP);
+    double cropYMax = MathUtil.clamp(maxY + CROP_BUFFER, -DEFAUlT_CROP, DEFAUlT_CROP);
+
+    LimelightHelpers.setCropWindow(name, cropXMin, cropXMax, cropYMin, cropYMax);
+  }
+
+  private void resetCropping() {
+    LimelightHelpers.setCropWindow(name, -DEFAUlT_CROP, DEFAUlT_CROP, -DEFAUlT_CROP, DEFAUlT_CROP);
   }
 }
