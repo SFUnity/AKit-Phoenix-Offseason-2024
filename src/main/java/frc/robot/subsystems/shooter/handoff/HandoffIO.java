b309bc0fd62a4ecdf3f86b81605caf2f1df17cb5
MrTinker64 <avi.micah@gmail.com>
2024-08-28 16:35:41 -0700

    make handoff io

---
+++ src/main/java/frc/robot/subsystems/shooter/handoff/HandoffIO.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.shooter.handoff;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface HandoffIO {
+  @AutoLog
+  public static class HandoffIOInputs {
+    public double appliedVolts = 0.0;
+    public double[] currentAmps = new double[] {};
+  }
+  
+  /** Updates the set of loggable inputs. */
+  default void updateInputs(HandoffIOInputs inputs) {}
+
+  /** Run open loop at the specified voltage. */
+  default void runVolts(double volts) {}
+
+  /** Stop handoff */
+  default void stop() {}
+}
