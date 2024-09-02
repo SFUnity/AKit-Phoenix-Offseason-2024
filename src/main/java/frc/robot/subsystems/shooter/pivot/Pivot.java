8de4d6153458ea63870c150ccc4fbe82c038f69e
MrTinker64 <avi.micah@gmail.com>
2024-09-02 07:59:07 -0700

    add missing import

--- src/main/java/frc/robot/subsystems/shooter/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/shooter/pivot/Pivot.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.FieldConstants;
 import frc.robot.util.EqualsUtil;
 import frc.robot.util.GeneralUtil;
